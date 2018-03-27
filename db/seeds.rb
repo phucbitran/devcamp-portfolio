4fbc0fc104ff01d61c6b2f5b078384e0de88509e
Tho Phuc <phucbitran@gmail.com>
2018-03-27 17:33:50 +0700

    Created seed file and added Faker gem file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+	Blog.create(title: "My Blog Post #{blog}",
+				body: Faker::Lorem.paragraphs)
+end
+
+9.times do 
+	Portfolio.create(
+		title: Faker::Lorem.sentence,
+		subtitle: Faker::Lorem.sentence,
+		body: Faker::Lorem.paragraphs,
+		main_image: "http://via.placeholder.com/600x400",
+		thumb_image: "http://via.placeholder.com/350x200")
+end
+5.times do |skill|
+	Skill.create(title: "My Blog Post #{skill}",
+				percent_utilized: 15 )
+end
