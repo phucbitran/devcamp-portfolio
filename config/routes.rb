06e682ea719c97dc0e8afd068f5862dce8fca428
Tho Phuc <phucbitran@gmail.com>
2018-03-27 23:36:08 +0700

    override resources routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
-  
-  resources :portfolios
+  root to: 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
+  resources :portfolios, except: [:show]
+  get 'portfolios/:id', to: 'portfolios#show', as: 'portfolio_show'
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
