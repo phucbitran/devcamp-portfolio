c2cba7714a6f81341f8852cc41a2cc18d2adc483
Tho Phuc <phucbitran@gmail.com>
2018-03-27 20:06:02 +0700

    integrated new and create actions for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,24 @@
 class PortfoliosController < ApplicationController
+	# before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
 	def index
 		@portfolio_item = Portfolio.all
+	end
+	def new
+		@portfolio_item = Portfolio.new
+	end
+	def create
+		@portfolio_item = Portfolio.new(portfolio_params)
+		if @portfolio_item.save
+			redirect_to portfolios_path, notice: 'Portfolio item was successfully created.'
+		else
+			render :new
+		end
+	end
+	private 
+	def set_portfolio
+		@portfolio_item = Portfolio.find(params[:id])
+	end
+	def portfolio_params
+		params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image)
 	end
 end
