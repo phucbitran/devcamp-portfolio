b88d636bae681c6304e6ce54842eb511b07e18fb
Tho Phuc <phucbitran@gmail.com>
2018-03-28 16:47:45 +0700

    implement scopes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   resources :portfolios, except: [:show]
   get 'portfolios/:id', to: 'portfolios#show', as: 'portfolio_show'
+  get 'angular-items', to: 'portfolios#angular'
   resources :blogs do
   	member do 
   		get :toggle_status
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,11 @@
 
 10.times do |blog|
 	Blog.create(title: "My Blog Post #{blog}",
-				body: Faker::Lorem.paragraphs)
+				body: Faker::Lorem.paragraphs,
+				topic_id: Topic.last.id)
 end
 
-9.times do 
+8.times do 
 	Portfolio.create(
 		title: Faker::Lorem.sentence,
 		subtitle: Faker::Lorem.sentence,
@@ -12,7 +13,21 @@ end
 		main_image: "http://via.placeholder.com/600x400",
 		thumb_image: "http://via.placeholder.com/350x200")
 end
+
+1.times do 
+	Portfolio.create(
+		title: Faker::Lorem.sentence,
+		subtitle: "Ruby on rails",
+		body: Faker::Lorem.paragraphs,
+		main_image: "http://via.placeholder.com/600x400",
+		thumb_image: "http://via.placeholder.com/350x200")
+end
+
 5.times do |skill|
 	Skill.create(title: "My Blog Post #{skill}",
 				percent_utilized: 15 )
+end
+
+3.times do 
+	Topic.create(title: Faker::Lorem.word)
 end
