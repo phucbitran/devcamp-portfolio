73388164aec87c7393beed04883f957cd439061e
Tho Phuc <phucbitran@gmail.com>
2018-03-28 15:17:21 +0700

    validate data

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  
-  root to: 'pages#home'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
   resources :portfolios, except: [:show]
   get 'portfolios/:id', to: 'portfolios#show', as: 'portfolio_show'
-  resources :blogs
+  resources :blogs do
+  	member do 
+  		get :toggle_status
+  	end
+  end
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
